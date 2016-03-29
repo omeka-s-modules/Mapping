57aeafba2adbb416828860fcc8e4138752411037
Jim Safley <jimsafley@gmail.com>
2016-03-29 01:25:52 -0400

    Add markers to separate layer group

--- asset/js/mapping-show.js
+++ asset/js/mapping-show.js
@@ -1,8 +1,6 @@
 $(document).ready( function() {
 
 var mappingMap = $('#mapping-map');
-var markers = mappingMap.data('markers');
-var markerMedia = mappingMap.data('markerMedia');
 var map = L.map('mapping-map').setView([0, 0], 1);
 var baseMaps = {
     'Streets': L.tileLayer.provider('OpenStreetMap.Mapnik'),
@@ -10,19 +8,20 @@ var baseMaps = {
     'Satellite': L.tileLayer.provider('Esri.WorldImagery'),
     'Terrain': L.tileLayer.provider('Esri.WorldShadedRelief')
 };
+var drawnItems = new L.FeatureGroup();
 var layerControl = L.control.layers(baseMaps);
 
 map.addLayer(baseMaps['Streets']);
+map.addLayer(drawnItems);
 map.addControl(layerControl);
 
-$.each(markers, function(index, data) {
+$.each(mappingMap.data('markers'), function(index, data) {
     var latLng = L.latLng(data['o-module-mapping:lat'], data['o-module-mapping:lng']);
     var marker = L.marker(latLng);
     var popupContent = $('.template.mapping-marker-popup-content[data-marker-id="' + data['o:id'] + '"]')
         .clone().removeClass('template');
     marker.bindPopup(popupContent[0]);
-    marker.addTo(map);
-
+    drawnItems.addLayer(marker);
 });
 
 // Switching sections changes map dimensions, so make the necessary adjustments.
