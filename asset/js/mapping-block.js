0b0b12d1dfc1f8d3d377228479de9a926a53ecd6
Jim Safley <jimsafley@gmail.com>
2016-05-06 13:33:17 -0400

    Improve code

--- asset/js/mapping-block.js
+++ asset/js/mapping-block.js
@@ -5,45 +5,17 @@ var mappingMaps = $('.mapping-map');
 mappingMaps.each(function() {
     var mappingMap = $(this);
 
+    // Initialize the map with default view.
     var defaultZoom = mappingMap.data('defaultZoom');
     var defaultLat = mappingMap.data('defaultLat');
     var defaultLng = mappingMap.data('defaultLng');
-    var wms = mappingMap.data('wms');
-    var markers = mappingMap.data('markers');
-    var baseMaps = {
-        'Streets': L.tileLayer.provider('OpenStreetMap.Mapnik'),
-        'Grayscale': L.tileLayer.provider('OpenStreetMap.BlackAndWhite'),
-        'Satellite': L.tileLayer.provider('Esri.WorldImagery'),
-        'Terrain': L.tileLayer.provider('Esri.WorldShadedRelief')
-    };
-    var groupedOverlays = {'Overlays': {
-        'No overlay': L.tileLayer.canvas()
-    }};
     var map = L.map(this, {
         center: [defaultLat, defaultLng],
         zoom: defaultZoom
     });
 
-    $.each(wms, function(index, data) {
-        wms = L.tileLayer.wms(data.base_url, {
-            layers: data.layers,
-            styles: data.styles,
-            format: 'image/png',
-            transparent: true,
-        }).addTo(map);
-        groupedOverlays['Overlays'][data.label] = wms;
-    });
-
-    map.addLayer(baseMaps['Streets']);
-    L.control.groupedLayers(baseMaps, groupedOverlays, {
-        exclusiveGroups: ['Overlays']
-    }).addTo(map);
-    $.each(groupedOverlays['Overlays'], function(index, data) {
-        map.removeLayer(groupedOverlays['Overlays'][index]);
-    });
-    map.addLayer(groupedOverlays['Overlays']['No overlay']);
-
-    $.each(markers, function(index, data) {
+    // Add markers to map.
+    $.each(mappingMap.data('markers'), function(index, data) {
         var latLng = L.latLng(data['o-module-mapping:lat'], data['o-module-mapping:lng']);
         var marker = L.marker(latLng);
         var popupContent = $('.mapping-marker-popup-content[data-marker-id="' + data['o:id'] + '"]');
@@ -53,6 +25,38 @@ mappingMaps.each(function() {
         }
         marker.addTo(map);
     });
+
+    // Add base map and WMS overlay layers to map.
+    var baseMaps = {
+        'Streets': L.tileLayer.provider('OpenStreetMap.Mapnik'),
+        'Grayscale': L.tileLayer.provider('OpenStreetMap.BlackAndWhite'),
+        'Satellite': L.tileLayer.provider('Esri.WorldImagery'),
+        'Terrain': L.tileLayer.provider('Esri.WorldShadedRelief')
+    };
+    var groupedOverlays = {
+        'Overlays': {
+            // Set an empty canvas for a "no overlay" option.
+            'No overlay': L.tileLayer.canvas()
+        }
+    };
+    $.each(mappingMap.data('wms'), function(index, data) {
+        wms = L.tileLayer.wms(data.base_url, {
+            layers: data.layers,
+            styles: data.styles,
+            format: 'image/png',
+            transparent: true,
+        }).addTo(map);
+        groupedOverlays['Overlays'][data.label] = wms;
+    });
+    L.control.groupedLayers(baseMaps, groupedOverlays, {
+        exclusiveGroups: ['Overlays']
+    }).addTo(map);
+    // Remove grouped overlays from the map so the user can select one at a time.
+    $.each(groupedOverlays['Overlays'], function(index, data) {
+        map.removeLayer(data);
+    });
+    map.addLayer(baseMaps['Streets']);
+    map.addLayer(groupedOverlays['Overlays']['No overlay']);
 });
 
 });
