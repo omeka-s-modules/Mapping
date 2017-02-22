a68d3cfddd9ade62c31c323808c64ace4c26c1d8
Jim Safley <jimsafley@gmail.com>
2017-02-22 10:57:44 -0500

    Limit to a reasonable amount of items that have markers
    Avoids reaching the server memory limit and improves client performance.

--- src/Controller/Site/IndexController.php
+++ src/Controller/Site/IndexController.php
@@ -16,6 +16,12 @@ class IndexController extends AbstractActionController
         if ($this->siteSettings()->get('browse_attached_items', false)) {
             $query['site_attachments_only'] = true;
         }
+
+        // Limit to a reasonable amount of items that have markers to avoid
+        // reaching the server memory limit and to improve client performance.
+        $query['limit'] = 5000;
+        $query['has_markers'] = true;
+
         $items = $this->api()->search('items', $query)->getContent();
         $itemIds = [];
         foreach ($items as $item) {
