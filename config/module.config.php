9fd2131d4c0d0d99268c6c98396be965173f0198
patrickmj <patrickmjchnm@gmail.com>
2018-04-09 13:28:16 -0400

    remove automapping and fix classes

--- config/module.config.php
+++ config/module.config.php
@@ -38,57 +38,7 @@ return [
     ],
     'csv_import' => [
         'mappings' => [
-            'items' => [
-                CsvMapping\CsvMapping::class,
-            ],
-        ],
-        'automapping' => [
-            'mapping_latitude' => [
-                'name' => 'map-lat',
-                'value' => 1,
-                'label' => 'Latitude' ,
-                'class' => 'mapping-module',
-            ],
-            'mapping_longitude' => [
-                'name' => 'map-lng',
-                'value' => 1,
-                'label' => 'Longitude' ,
-                'class' => 'mapping-module',
-            ],
-            'mapping_latitude_longitude' => [
-                'name' => 'map-latlng',
-                'value' => 1,
-                'label' => 'Latitude/Longitude' ,
-                'class' => 'mapping-module',
-            ],
-            'mapping_default_latitude' => [
-                'name' => 'default-lat',
-                'value' => 1,
-                'label' => 'Default latitude' ,
-                'class' => 'mapping-module-defaults',
-            ],
-            'mapping_default_longitude' => [
-                'name' => 'default-lng',
-                'value' => 1,
-                'label' => 'Default longitude' ,
-                'class' => 'mapping-module-defaults',
-            ],
-            'mapping_default_zoom' => [
-                'name' => 'default-zoom',
-                'value' => 1,
-                'label' => 'Default zoom' ,
-                'class' => 'mapping-module-defaults',
-            ],
-        ],
-        'user_settings' => [
-            'csv_import_automap_user_list' => [
-                'latitude' => 'mapping_latitude',
-                'longitude' => 'mapping_longitude',
-                'latitude/longitude' => 'mapping_latitude_longitude',
-                'default latitude' => 'mapping_default_latitude',
-                'default longitude' => 'mapping_default_longitude',
-                'default zoom' => 'mapping_default_zoom',
-            ],
+            'items' => [ CsvMapping\CsvMapping::class ],
         ],
     ],
     'omeka2_importer_classes' => [
