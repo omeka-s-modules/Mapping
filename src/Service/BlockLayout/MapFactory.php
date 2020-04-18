cf3aead2d53a198f47d93da46056678f58618e02
Jim Safley <jimsafley@gmail.com>
2020-04-18 16:30:16 -0400

    Add map block that adds items by query
    (ref #60)

--- src/Service/BlockLayout/MapFactory.php
+++ src/Service/BlockLayout/MapFactory.php
@@ -3,15 +3,20 @@ namespace Mapping\Service\BlockLayout;
 
 use Interop\Container\ContainerInterface;
 use Mapping\Site\BlockLayout\Map;
+use Mapping\Site\BlockLayout\MapQuery;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 class MapFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        return new Map(
-            $services->get('Omeka\HtmlPurifier'),
-            $services->get('Omeka\ModuleManager')
-        );
+        $htmlPurifier = $services->get('Omeka\HtmlPurifier');
+        $moduleManager = $services->get('Omeka\ModuleManager');
+        switch ($requestedName) {
+            case 'mappingMapQuery':
+                return new MapQuery($htmlPurifier, $moduleManager);
+            default:
+                return new Map($htmlPurifier, $moduleManager);
+        }
     }
 }
