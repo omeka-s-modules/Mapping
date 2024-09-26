e54fb542ad7840f0a5d04989de73e21fb83a897e
Jim Safley <jimsafley@gmail.com>
2024-09-26 13:55:50 -0400

    Optimize resource show

--- asset/js/mapping-show.js
+++ asset/js/mapping-show.js
@@ -20,6 +20,14 @@ const featuresPoly = L.deflate({
     greedyCollapse: false // Must set to false or small poly features will not be inflated at high zoom.
 });
 
+// Set the initial view to geographical center of world.
+map.setView([20, 0], 2);
+// Detect a map interaction by the user.
+let mapInteraction = false;
+map.on('zoomend', function() {
+    mapInteraction = true;
+});
+
 // Set base maps.
 let defaultProvider;
 try {
@@ -43,33 +51,66 @@ if (mappingData && mappingData['o-module-mapping:bounds'] !== null) {
     defaultBounds = [southWest, northEast];
 }
 
+// Load features asynchronously.
+const featuresUrl = mappingMap.data('featuresUrl');
 const featurePopupContentUrl = mappingMap.data('featurePopupContentUrl');
-$('.mapping-feature-popup-content').each(function() {
-    const popup = $(this).clone().show();
-    const geography = popup.data('feature-geography');
-    L.geoJSON(geography, {
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(popup[0]);
-            layer.on('popupopen', function() {
-                $.get(featurePopupContentUrl, {feature_id: popup.data('featureId')}, function(data) {
-                    popup.html(data);
+const getFeaturesQuery = {
+    features_page: 0,
+    items_query: JSON.stringify(mappingMap.data('itemsQuery')),
+    features_query: JSON.stringify(mappingMap.data('featuresQuery')),
+};
+let featuresPage = 0;
+const loadFeaturePopups = function() {
+    getFeaturesQuery.features_page = ++featuresPage;
+    $.get(featuresUrl, getFeaturesQuery)
+        .done(function(featuresData) {
+            if (!featuresData.length) {
+                // Stop fetching features after all have been loaded.
+                if (!mapInteraction) {
+                    // Fit to bounds only if there was no map interaction.
+                    const bounds = features.getBounds();
+                    if (bounds.isValid()) {
+                        map.fitBounds(bounds, {padding: [50, 50]});
+                    } else {
+                        map.setView([0, 0], 1);
+                    }
+                }
+                return;
+            }
+            featuresData.forEach((featureData) => {
+                const featureId = featureData[0];
+                const resourceId = featureData[1];
+                const featureGeography = featureData[2];
+                L.geoJSON(featureGeography, {
+                    onEachFeature: function(feature, layer) {
+                        const popup = L.popup();
+                        layer.bindPopup(popup);
+                        if (featurePopupContentUrl) {
+                            layer.on('popupopen', function() {
+                                $.get(featurePopupContentUrl, {feature_id: featureId}, function(popupContent) {
+                                    popup.setContent(popupContent);
+                                });
+                            });
+                        }
+                        switch (feature.type) {
+                            case 'Point':
+                                featuresPoint.addLayer(layer);
+                                break;
+                            case 'LineString':
+                            case 'Polygon':
+                                layer.on('popupopen', function() {
+                                    map.fitBounds(layer.getBounds());
+                                });
+                                featuresPoly.addLayer(layer);
+                                break;
+                        }
+                    }
                 });
             });
-            switch (feature.type) {
-                case 'Point':
-                    featuresPoint.addLayer(layer);
-                    break;
-                case 'LineString':
-                case 'Polygon':
-                    layer.on('popupopen', function() {
-                        map.fitBounds(layer.getBounds(), {padding: [50, 50]});
-                    });
-                    featuresPoly.addLayer(layer);
-                    break;
-            }
-        }
-    });
-});
+            loadFeaturePopups();
+        });
+};
+loadFeaturePopups();
 
 features.addLayer(featuresPoint)
     .addLayer(featuresPoly);
--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -6,6 +6,31 @@ use Laminas\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+    public function getFeaturesAction()
+    {
+        $itemsQuery = json_decode($this->params()->fromQuery('items_query'), true);
+        $itemsQuery['has_features'] = true;
+        $itemsQuery['limit'] = 100000;
+        $itemIds = $this->api()->search('items', $itemsQuery, ['returnScalar' => 'id'])->getContent();
+
+        $featuresQuery = json_decode($this->params()->fromQuery('features_query'), true);
+        $featuresQuery['page'] = $this->params()->fromQuery('features_page');
+        $featuresQuery['per_page'] = 10000;
+        $featuresQuery['item_id'] = $itemIds;
+        $featureResponse = $this->api()->search('mapping_features', $featuresQuery);
+
+        $features = [];
+        foreach ($featureResponse->getContent() as $feature) {
+            $features[] = [
+                $feature->id(),
+                $feature->item()->id(),
+                $feature->geography(),
+            ];
+        }
+
+        return new \Laminas\View\Model\JsonModel($features);
+    }
+
     public function getFeaturePopupContentAction()
     {
         $featureId = $this->params()->fromQuery('feature_id');
