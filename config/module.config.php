902fac4ab642afef6e096fe2428314dab4d824c4
Jim Safley <jimsafley@gmail.com>
2024-11-06 15:17:52 -0500

    Register ViewJsonStrategy

--- config/module.config.php
+++ config/module.config.php
@@ -33,6 +33,9 @@ return [
         'template_path_stack' => [
             dirname(__DIR__) . '/view',
         ],
+        'strategies' => [
+            'ViewJsonStrategy',
+        ],
     ],
     'view_helpers' => [
         'invokables' => [
