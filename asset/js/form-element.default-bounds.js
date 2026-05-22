cadd7cd622927e5048c7c8ab416333d61282c8b4
Jim Safley <jimsafley@gmail.com>
2026-05-22 13:02:25 -0400

    Add configurable map defaults to global and user settings
    Adds basemap provider, min/max zoom, and default bounds to global and
    user settings. A new DefaultBounds element renders a Leaflet map for
    setting bounds visually. The item edit map reads all four settings via
    fallbackSetting() and applies them as the initial basemap, zoom
    constraints, and default view.

---
+++ asset/js/form-element.default-bounds.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+$('.mapping-default-bounds').each(function() {
+    const container = $(this);
+    const hiddenInput = container.find('input[type="hidden"]');
+    const mapEl = container.find('.mapping-default-bounds-map')[0];
+
+    const initMap = function() {
+        const map = L.map(mapEl, {
+            worldCopyJump: true,
+        });
+
+        L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+
+        const val = hiddenInput.val();
+        if (val) {
+            const b = val.split(',');
+            map.fitBounds([[b[1], b[0]], [b[3], b[2]]]);
+        } else {
+            map.setView([20, 0], 2);
+        }
+
+        map.addControl(new L.Control.DefaultView(
+            function() {
+                hiddenInput.val(map.getBounds().toBBoxString());
+            },
+            function() {
+                const b = hiddenInput.val().split(',');
+                map.fitBounds([[b[1], b[0]], [b[3], b[2]]]);
+            },
+            function() {
+                hiddenInput.val('');
+                map.setView([20, 0], 2);
+            },
+            {noInitialDefaultView: !val}
+        ));
+    };
+
+    // Defer init until the section is visible so Leaflet gets real dimensions.
+    const section = container.closest('.section');
+    if (section.length && !section.is(':visible')) {
+        section.one('o:section-opened', initMap);
+    } else {
+        initMap();
+    }
+});
+
+});
---
+++ src/View/Helper/FormDefaultBounds.php
@@ -0,0 +1,30 @@
+<?php
+namespace Mapping\View\Helper;
+
+use Laminas\Form\ElementInterface;
+use Laminas\Form\View\Helper\AbstractHelper;
+
+class FormDefaultBounds extends AbstractHelper
+{
+    public function __invoke(ElementInterface $element)
+    {
+        return $this->render($element);
+    }
+
+    public function render(ElementInterface $element)
+    {
+        $view = $this->getView();
+
+        $view->headLink()->appendStylesheet($view->assetUrl('node_modules/leaflet/dist/leaflet.css', 'Mapping'));
+        $view->headScript()->appendFile($view->assetUrl('node_modules/leaflet/dist/leaflet.js', 'Mapping'));
+        $view->headScript()->appendFile($view->assetUrl('node_modules/leaflet-providers/leaflet-providers.js', 'Mapping'));
+        $view->headScript()->appendFile($view->assetUrl('js/control.default-view.js', 'Mapping'));
+        $view->headScript()->appendFile($view->assetUrl('js/form-element.default-bounds.js', 'Mapping'));
+
+        return sprintf(
+            '<div class="mapping-default-bounds"><input type="hidden" name="%s" value="%s"><div class="mapping-default-bounds-map" style="height:300px;"></div></div>',
+            $view->escapeHtml($element->getName()),
+            $view->escapeHtml($element->getValue() ?? '')
+        );
+    }
+}
